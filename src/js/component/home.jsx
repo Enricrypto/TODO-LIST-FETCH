c6d6b900347576a0256ab7b907fb909bfc94acbe
Enricrypto <eibarraf@gmail.com>
2023-03-13 15:06:58 +0000

    changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,46 +6,88 @@ const Home = () => {
 
 	const [state, setState] = useState([]);
 	const [load, setLoad] = useState(false);
-	const [newTask, setnewTask] = useState("");
+	const [newTask, setNewTask] = useState("");
+	const [taskDone, setTaskDone] = useState(); 
 
-const getData = async () =>{
-	try {
-		const response = await fetch(URL, { method: "GET" });
-		const data = await response.json();
-		setState(data);
-		console.log(data);
-		setLoad(true);
-	  } catch (err) {
-		console.log("err");
-	  }
-  }
+	const getData = async () => {
+		try {
+			setLoad(true);
+			const response = await fetch(URL, { method: "GET" });
+			const data = await response.json();
+			setState(data);
+			setLoad(false);
+		} catch (err) {
+			console.log("err");
+		}
+	}
 
-useEffect(() => {
-	getData()
+	useEffect(() => {
+		getData()
 	}, []);
 
-const addNewTask = async () => {
-	try {
-	  const data =[...state, {label: newTask, done: false}]
-	  const response = await fetch(URL, { method: "PUT", body: JSON.stringify(data), headers:{"Content-Type": "application/json"}});
-	  console.log(response);
-	  getData();
-  } catch (err) {
-	  console.log("err");
-	}
-  }; 
+	const addNewTask = async () => {
+		try {
+			setLoad(true);
+			const data = [...state, { label: newTask, done: false }]
+			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(data), headers: { "Content-Type": "application/json" } });
+			console.log(response);
+			getData();
+			setLoad(false);
+		} catch (error) {
+			console.log("err");
+		}
+	};
 
-const handleChange = (e) => {
-	setnewTask(e.target.value);
-}
+	const doneTask = async (indice) => {
+		try {
+			const newState = [...state];
+			setTaskDone(newState[indice]["done"]); 
+			console.log("aqui", !taskDone, taskDone); 
+			newState[indice].done = !taskDone;
+			// console.log(state, taskDone, indice); 
+			setLoad(true);
+			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(newState), headers: { "Content-Type": "application/json" } });
+			console.log(response);
+			await getData();
+			setLoad(false);
+		} catch (err) {
+			console.log("err");
+		}
+	};
+
+	const deleteTask = async (indice) => {
+		try {
+			const newState = state.filter((elem, index) => {
+				return indice !== index
+			})
+			setLoad(true);
+			const response = await fetch(URL, { method: "PUT", body: JSON.stringify(newState), headers: { "Content-Type": "application/json" } });
+			console.log(response);
+			await getData();
+			setLoad(false);
+		} catch (err) {
+			console.log("err");
+		}
+	};
+
+
 
 	return (
 		<div className="text-center">
-			<input onChange={handleChange} type="text" placeholder="add task"/>
-		<button onClick={addNewTask}>Add New Task</button>
-		{ load ? state.map(task => { 
-			return <h1 key={task.label}> {task.label} </h1>}) 
-			: <h1>Loading...</h1>}
+			<div className="input-btn">
+			<input className="input" onChange={(e) => setNewTask(e.target.value)} type="text" placeholder="write down your task" />
+			<button className="btn btn-primary add-button" onClick={ addNewTask }>Add New Task</button>
+			{load ? <div className="spinner spinner-border m-5" role="status">
+				<span className="visually-hidden">Loading...</span>
+			</div>
+				: state.map((task, index) => {
+					return <div className="card mt-3" key={index}>
+						<h1 key={task.label}> {task.label} </h1>
+						{task.done ? <button className="btn btn-secondary" onClick={() => doneTask(index) }>Mark as Undone</button> : <button className="btn btn-success" onClick={() => doneTask(index) }>Mark as Done</button>}
+						<button className="btn btn-danger mt-1" onClick={() => { deleteTask(index) }}>Delete Task</button>
+					</div>
+				})}
+			</div>
 		</div>
 	);
 };
